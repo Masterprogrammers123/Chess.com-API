46747fe54058c0e20736c876fb2dce3050b301d8
zackprogrammer <63200308+zackprogrammer@users.noreply.github.com>
2021-02-24 17:16:24 -0500

    Update: Added comments 👍
    I added comments.
    
    Plz read: Just an extra message for tim my grammer is horrbile so if u see an erorrs of grammer or u want it a sepcifc format u can just accept the pull request and change what u need to change. Remove this message later

--- tutorial.py
+++ tutorial.py
@@ -1,33 +1,44 @@
+#imports
+
 from chessdotcom import get_leaderboards, get_player_stats, get_player_game_archives
 import pprint
 import requests
-
+#Set up pprints
 printer = pprint.PrettyPrinter()
-
+#prints leaderbaord
 def print_leaderboards():
+	#makes a data vairable for the data
 	data = get_leaderboards().json
+	#another variable for the data keys i think.
 	categories = data.keys()
-
+        #prints categorie	
 	for category in categories:
 		print('Category:', category)
 		for idx, entry in enumerate(data[category]):
 			print(f'Rank: {idx + 1} | Username: {entry["username"]} | Rating: {entry["score"]}')
 
-
+# Gets the players rating
 def get_player_rating(username):
-	data = get_player_stats(username).json
+	#data variable contains the data of the users		      
+	data = get_player_stats(username)
+	# It gets cateogires		      
 	categories = ['chess_blitz', 'chess_rapid', 'chess_bullet']
 	for category in categories:
+		# prints out the info	      
 		print('Category:', category)
 		print(f'Current: {data[category]["last"]["rating"]}')
 		print(f'Best: {data[category]["best"]["rating"]}')
 		print(f'Best: {data[category]["record"]}')
-
+#gets most recent game
 def get_most_recent_game(username):
+	# data variable gets the players history of games of chess.	      
 	data = get_player_game_archives(username).json
 	url = data['archives'][-1]
 	games = requests.get(url).json()
 	game = games['games'][-1]
+	# Prints it out	      
 	printer.pprint(game)
 
 get_most_recent_game('timruscica')
+		      
+		      
